1a8070554a655de837205fcc9e5b13957a22fe62
Kavya Verma <kavyaverma@ucsb.edu>
2025-01-28 12:05:13 -0800

    Kavya - added navbar and basic navigation

---
+++ hello-world/src/views/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+import Nav from '../components/Navigation/Nav';
+
+export default function ContactUs() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  )
+}
